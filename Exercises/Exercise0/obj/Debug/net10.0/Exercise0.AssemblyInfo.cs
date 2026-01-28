f659a12d40bf65c1700b1ffd1be986dbdf41f748
panchaldev713-bit <panchaldev713@gmail.com>
2026-01-28 11:57:56 -0500

    add Exercise2

--- Exercises/Exercise0/obj/Debug/net10.0/Exercise0.AssemblyInfo.cs
+++ Exercises/Exercise0/obj/Debug/net10.0/Exercise0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01bd50c3d435cb0e6c7efacb06db57d29babba14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00237821e07b404650f422668916ef0d3a2ce30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Exercises/Exercise2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Let me calculte for you");
+Console.WriteLine("Enter a number");
+string number = Console.ReadLine();
+int number1 = int.Parse(number);
+Console.WriteLine("Enter another number");
+number = Console.ReadLine();
+int number2 = int.Parse(number);
+int sum = number1 + number2;
+Console.WriteLine($"The sum of {number1} and {number2} is {sum}");
