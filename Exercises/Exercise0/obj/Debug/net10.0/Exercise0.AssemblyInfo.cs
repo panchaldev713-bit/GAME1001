c6d48becf840f1f58b29efa5586c51ef173c45ba
panchaldev713-bit <panchaldev713@gmail.com>
2026-02-03 15:56:16 -0500

    Added more equations
    Added Addition (+)
    
    Added Subtraction (-)
    
    Added Multiplication (*)
    
    Added Division (/)

--- Exercises/Exercise0/obj/Debug/net10.0/Exercise0.AssemblyInfo.cs
+++ Exercises/Exercise0/obj/Debug/net10.0/Exercise0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00237821e07b404650f422668916ef0d3a2ce30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f659a12d40bf65c1700b1ffd1be986dbdf41f748")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Exercises/Exercise2/Program.cs
+++ Exercises/Exercise2/Program.cs
@@ -1,10 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Let me calculte for you");
+Console.WriteLine("enter the opration: + - * /");
+string userChoise = Console.ReadLine();
 Console.WriteLine("Enter a number");
 string number = Console.ReadLine();
 int number1 = int.Parse(number);
 Console.WriteLine("Enter another number");
 number = Console.ReadLine();
 int number2 = int.Parse(number);
-int sum = number1 + number2;
-Console.WriteLine($"The sum of {number1} and {number2} is {sum}");
+
+if(userChoise =="+")
+{
+    int sum = number1 + number2;
+    Console.WriteLine($"The sum of {number1} and {number2} is {sum}");
+    
+}
+else if(userChoise =="-")
+{
+    int answer = number1 - number2;
+    Console.WriteLine($"The result of {number1} and {number2} is {answer}");
+    }
+else if(userChoise =="*")
+{
+    int answer = number1 * number2;
+    Console.WriteLine($"The multiplication of {number1} and {number2} is {answer}");
+    }
+else if(userChoise =="/")
+{
+    int answer = number1 / number2;
+    Console.WriteLine($"The divition of {number1} and {number2} is {answer}");
+    }
